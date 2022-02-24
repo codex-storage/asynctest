c4b23d73a9fdeabbf43a6c5f34417fc64ee6a9d3
Eric Mastro <eric.mastro@gmail.com>
2022-02-24 22:15:45 +1100

    feat: add support for suite before and after
    `before` executes async code before all tests in the suite.
    `after` executes async code after all tests in the suite.

--- asynctest/templates.nim
+++ asynctest/templates.nim
@@ -1,5 +1,17 @@
 template suite*(name, body) =
+
   suite name:
+
+    # Runs before all tests in the suite
+    template setupAll(setupAllBody) {.used.} =
+      let b = proc {.async.} = setupAllBody
+      waitFor b()
+
+    # Runs after all tests in the suite
+    template teardownAll(teardownAllBody) {.used.} =
+      template teardownAllIMPL: untyped {.inject.} =
+        let a = proc {.async.} = teardownAllBody
+        waitFor a()
 
     template setup(setupBody) {.used.} =
       setup:
@@ -11,7 +23,12 @@ template suite*(name, body) =
         let asyncproc = proc {.async.} = teardownBody
         waitFor asyncproc()
 
-    let suiteproc = proc = body # Avoids GcUnsafe2 warnings with chronos
+    let suiteproc = proc = # Avoids GcUnsafe2 warnings with chronos
+      body
+
+      when declared(teardownAllIMPL):
+        teardownAllIMPL()
+
     suiteproc()
 
 template test*(name, body) =
--- testmodules/stdlib/testbody.nim
+++ testmodules/stdlib/testbody.nim
@@ -15,3 +15,17 @@ suite "test async proc":
   test "async test":
     # invoke await in tests:
     await someAsyncProc()
+
+suite "test async setupAll and teardownAll, allow multiple suites":
+
+  setupAll:
+    # invoke await in the test setup:
+    await someAsyncProc()
+
+  teardownAll:
+    # invoke await in the test teardown:
+    await someAsyncProc()
+
+  test "async test":
+    # invoke await in tests:
+    await someAsyncProc()
