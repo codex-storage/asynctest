4f89a82a496d87d2cd9a187254eab12e25c86fe9
Mark Spanbroek <mark@spanbroek.net>
2023-12-21 10:17:17 +0100

    Fix BareExcept warnings when using stdlib unittest with chronos v4

--- asynctest/private/chronos/unittest/runasync.nim
+++ asynctest/private/chronos/unittest/runasync.nim
@@ -4,7 +4,14 @@ import pkg/chronos/config
 when compiles(config.chronosHandleException): # detect chronos v4
 
   template runAsync*(body): untyped =
-    let asyncProc = proc {.async: (raises: [Exception]).} = body
+    # The unittest module from stdlib can still raise a bare Exception,
+    # so we allow chronos to convert it to an AsyncExceptionError, and
+    # we disable the ensuing BareExcept warning.
+    {.push warning[BareExcept]:off.}
+    let asyncProc =
+      proc {.async: (handleException: true, raises: [AsyncExceptionError]).} =
+        body
+    {.pop.}
     waitFor asyncProc()
 
 else:
