0fac3d3988b0830fbd42e7b3525d474db724f707
Mark Spanbroek <mark@spanbroek.net>
2023-12-21 13:04:33 +0100

    Add helpful compiler errors on old import paths

---
+++ asynctest.nim
@@ -0,0 +1,4 @@
+{.error:
+  "As of version 0.5.0 you need to import either " &
+  "asynctest/asyncdispatch/unittest or " &
+  "asynctest/chronos/unittest depending on your choice of async framework".}
