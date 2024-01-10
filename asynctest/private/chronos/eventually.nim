d22828cb8b620202c05d5dff84c132128151935e
Mark Spanbroek <mark@spanbroek.net>
2024-01-10 13:01:34 +0100

    Fix: ensure that `eventually` works when std/times is imported

--- asynctest/private/chronos/eventually.nim
+++ asynctest/private/chronos/eventually.nim
@@ -1,6 +1,7 @@
 import pkg/chronos
 
 template eventually*(expression: untyped, timeout=5000): bool =
+  bind Moment, now, milliseconds
 
   proc eventually: Future[bool] {.async.} =
     let endTime = Moment.now() + timeout.milliseconds
--- testmodules/chronosv4/test.nim
+++ testmodules/chronosv4/test.nim
@@ -2,3 +2,10 @@ import pkg/asynctest/chronos/unittest
 
 include ../common/testbody
 include ../common/testfail
+
+import std/times
+
+suite "eventually and std/times":
+
+  test "compiles when both chronos and std/times are imported":
+    check eventually true
