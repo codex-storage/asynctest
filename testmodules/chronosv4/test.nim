d60ffb9792234986e14f88dcc21a15437d0ea212
gmega <giuliano.mega@gmail.com>
2024-01-11 16:54:33 -0300

    add exception handling for Chronos V4

--- testmodules/chronosv4/test.nim
+++ testmodules/chronosv4/test.nim
@@ -9,3 +9,10 @@ suite "eventually and std/times":
 
   test "compiles when both chronos and std/times are imported":
     check eventually true
+
+  test "compiles when the expression handed to eventually can raise Exception":
+    proc aProc(): bool {.raises: [Exception].} = raise newException(
+      Exception, "an exception")
+    check:
+      compiles:
+        discard eventually aProc()
