12c356672d26053ddc4c1443491e9eee65ec03a2
gmega <giuliano.mega@gmail.com>
2024-01-11 16:54:33 -0300

    add exception handling for Chronos V4

--- asynctest/private/chronos/eventually.nim
+++ asynctest/private/chronos/eventually.nim
@@ -1,9 +1,19 @@
 import pkg/chronos
+import pkg/chronos/config
+
+template eventuallyProcSignature(body: untyped): untyped =
+  when compiles(config.chronosHandleException):
+    proc eventually: Future[bool] {.async: (handleException: true,
+        raises: [AsyncExceptionError, CancelledError]).} =
+      body
+  else:
+    proc eventually: Future[bool] {.async.} =
+      body
 
 template eventually*(expression: untyped, timeout=5000): bool =
   bind Moment, now, milliseconds
 
-  proc eventually: Future[bool] {.async.} =
+  eventuallyProcSignature:
     let endTime = Moment.now() + timeout.milliseconds
     while not expression:
       if endTime < Moment.now():
